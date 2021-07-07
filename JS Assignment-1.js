5ef5a13abc437537b8beca4b44edba69ff2ea8d8
Yash07-coder <80715401+Yash07-coder@users.noreply.github.com>
2021-07-07 13:43:28 +0530

    Update and rename README.md to JS Assignment-1.js

---
+++ JS Assignment-1.js
@@ -0,0 +1,15 @@
+/* Day-2 Javascript Assignment
+ Program Done By: Yash Rawat  */
+console.log("Horizontal Printing of '*' 5 Times");
+let char="";
+for (i=1; i<=5; i++)
+{
+  char+="* ";
+}
+console.log(char);
+
+console.log("Vertical Printing of '*' 5 Times");
+for(i=1; i<=5; i++)
+{
+  console.log("* ");
+}
